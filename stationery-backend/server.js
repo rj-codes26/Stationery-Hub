32983204df3718d8190502d98a6ef3591bc3bea9
Rajat Prajapati <prajapatirajatkumar1@gmail.com>
2026-06-22 13:57:52 +0530

    Ready for Render deployment

--- stationery-backend/server.js
+++ stationery-backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const connectDB = require("./config/db");
 
@@ -10,7 +12,7 @@ app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
