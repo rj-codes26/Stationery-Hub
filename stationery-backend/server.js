ecc1985628eebe1fd6ed0088636168eb80ade800
Rajat Prajapati <prajapatirajatkumar1@gmail.com>
2026-06-22 14:28:51 +0530

    Connected product routes

--- stationery-backend/server.js
+++ stationery-backend/server.js
@@ -2,19 +2,27 @@ require("dotenv").config();
 
 const express = require("express");
 const connectDB = require("./config/db");
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
+// Middleware
+app.use(express.json());
+
+// Product Routes
+app.use("/products", productRoutes);
+
 app.get("/", (req, res) => {
   res.send("API Running");
 });
+
 app.get("/test", (req, res) => {
   res.json({
     success: true,
-    message: "Backend working"
+    message: "Backend working",
   });
 });
 
